1897f3747a3e4bd2e384976d45a0e00d8a785717
Jonathan Hsieh <jonathanh1386@gmail.com>
2017-06-26 18:25:43 -0700

    botdata turned into configuration.json and cleaned up config checking code

---
+++ MonkeyBot/Config.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Discord;
+using Newtonsoft.Json;
+
+namespace MonkeyBot
+{
+    public class Config
+    {
+        public static string FilePath { get; } = "config/configuration.json";
+
+        public string BotToken { get; set; }
+        public string BotPrefix { get; set; }
+
+        public static void EnsureExists()
+        {
+            if (!File.Exists(FilePath))
+            {
+                Program.Print(FilePath + " not found!", ConsoleColor.DarkRed);
+                string path = Path.GetDirectoryName(FilePath);
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                var con = new Config();
+
+                Program.Print("Please enter BotToken: ");
+                string bottoken = Console.ReadLine();
+
+                Program.Print("Please enter Prefix: ");
+                string botprefix = Console.ReadLine();
+
+                con.BotToken = bottoken;
+                con.BotPrefix = botprefix;
+                con.Save();
+            }
+
+            Program.Print("Configuration Loaded", ConsoleColor.Green);
+        }
+
+        public void Save()
+        => File.WriteAllText(FilePath, ToJson());
+
+        public static Config Load()
+        {
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(FilePath));
+        }
+
+        public string ToJson()
+            => JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
--- MonkeyBot/Program.cs
+++ MonkeyBot/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
 
 namespace MonkeyBot
 {
@@ -11,37 +14,77 @@ namespace MonkeyBot
 
         public async Task MainAsync()
         {
+            await Log(new LogMessage(LogSeverity.Verbose, "", "Bot Started..."));
             Console.ForegroundColor = ConsoleColor.White;
-        }
 
-        private Task Log(LogMessage msg, LogSeverity logSev)
+            Config.EnsureExists();
+            Config botdata = Config.Load();
+
+            // Check if the .json file contains "BotPrefix"
+            var client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                MessageCacheSize = 1000,
+            });
+
+
+            client.Log += Log;
+
+            await Log(new LogMessage(LogSeverity.Info, "", "BotToken: " + botdata.BotToken + " located!"));
+            Console.WriteLine("");
+
+            try
+            {
+                // Login to Dicords - Connects Bot
+                await client.LoginAsync(TokenType.Bot, botdata.BotToken);
+                await client.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                // Will throw "401: Unauthorized" if the BotToken is invalid
+                await Log(new LogMessage(LogSeverity.Critical, "", ex.Message));
+                await Log(new LogMessage(LogSeverity.Error, "", "Possibly invalid token?"));
+            }
+
+            await Task.Delay(-1);
+        }
+        /// <summary>
+        /// Logs data to the Console
+        /// </summary>
+        /// <param name="msg">LogMessage object to describe log data</param>
+        private static Task Log(LogMessage msg)
         {
             switch (msg.Severity)
             {
-                    case LogSeverity.Info:
-                        Print("INFO: " + msg.Message, ConsoleColor.Cyan);
-                        break;
-                    case LogSeverity.Verbose:
-                        Print("VERBOSE: " + msg.Message, ConsoleColor.Cyan);
-                        break;
-                    case LogSeverity.Debug:
-                        Print("DEBUG: " + msg.Message, ConsoleColor.DarkGreen);
-                        break;
-                    case LogSeverity.Warning:
-                        Print("WARN: " + msg.Message, ConsoleColor.Yellow);
-                        break;
-                    case LogSeverity.Error:
-                        Print("ERROR: " + msg.Message, ConsoleColor.DarkRed);
-                        break;
-                    case LogSeverity.Critical:
-                        Print("CRITICAL: " + msg.Message, ConsoleColor.Red);
-                        break;
+                case LogSeverity.Info:
+                    Print(DateTime.Now + "\t" + "INFO: " + msg.Message, ConsoleColor.Cyan);
+                    break;
+                case LogSeverity.Verbose:
+                    Print(DateTime.Now + "\t" + "VERBOSE: " + msg.Message, ConsoleColor.Cyan);
+                    break;
+                case LogSeverity.Debug:
+                    Print(DateTime.Now + "\t" + "DEBUG: " + msg.Message, ConsoleColor.DarkGreen);
+                    break;
+                case LogSeverity.Warning:
+                    Print(DateTime.Now + "\t" + "WARN: " + msg.Message, ConsoleColor.Yellow);
+                    break;
+                case LogSeverity.Error:
+                    Print(DateTime.Now + "\t" + "ERROR: " + msg.Message, ConsoleColor.DarkRed);
+                    break;
+                case LogSeverity.Critical:
+                    Print(DateTime.Now + "\t" + "CRITICAL: " + msg.Message, ConsoleColor.Red);
+                    break;
             }
 
             return Task.CompletedTask;
         }
 
-        private void Print(string msg, ConsoleColor color)
+        /// <summary>
+        /// Simple print data to Console
+        /// </summary>
+        /// <param name="msg">String to print to the Console</param>
+        /// <param name="color">Color of print</param>
+        public static void Print(string msg, ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(msg);
